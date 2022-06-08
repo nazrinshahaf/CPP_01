d53867b0fb6fe8c6ce431ae5d9d48d36966a4614
Nazrin shah Fernandez <nfernand@u82z06s01.42kl.edu.my>
2022-06-08 15:57:27 +0800

    Changed ex01 to show index of zombie for easier visualization

--- ex01/zombieHorde.cpp
+++ ex01/zombieHorde.cpp
@@ -6,7 +6,7 @@
 /*   By: nfernand <nfernand@student.42kl.edu.m      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/14 14:26:36 by nfernand          #+#    #+#             */
-/*   Updated: 2022/03/14 14:46:36 by nfernand         ###   ########.fr       */
+/*   Updated: 2022/06/08 15:56:53 by nfernand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,19 @@
 #include <iostream>
 #include <string>
 
+//just for purposes of testing i wrote this after finishing all cpp modules
+#include <sstream>
+std::string intToString(int n)
+{
+	std::ostringstream ss;
+	ss << n;
+	return ss.str();
+}
+
 Zombie	*zombieHorde(int N, std::string name)
 {
 	Zombie	*firstZombie = new Zombie[N];
 	for (int i = 0; i < N; i++)
-		firstZombie[i].setName(name);
+		firstZombie[i].setName(name + " " + intToString(i));
 	return (firstZombie);
 }
