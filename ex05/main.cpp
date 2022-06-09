cca84fe3a77bc2811368a216403126897157cd54
Nazrin shah Fernandez <nfernand@u82z06s01.42kl.edu.my>
2022-06-09 14:48:03 +0800

    clean up ex05. flcean

--- ex05/main.cpp
+++ ex05/main.cpp
@@ -6,11 +6,20 @@
 /*   By: nfernand <nfernand@student.42kl.edu.m      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/18 12:28:31 by nfernand          #+#    #+#             */
-/*   Updated: 2022/03/18 16:58:34 by nfernand         ###   ########.fr       */
+/*   Updated: 2022/06/09 14:47:37 by nfernand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+#include <time.h>
+#include <random>
+
 #include "Harl.hpp"
+
+#include "colours.h"
+
+using std::cout;
+using std::endl;
 
 int	main()
 {
@@ -20,4 +29,22 @@ int	main()
 	Harl.complain("info");
 	Harl.complain("warning");
 	Harl.complain("error");
+
+	cout << endl;
+	cout << MAGENTA "Generating random complain status..." RESET << endl;
+	srand(time(0));
+
+	string	complainStatus[] = 
+	{
+		"debug",
+		"info",
+		"warning",
+		"error"
+	};
+	for (int i = 0 ; i < 4; i++)
+		Harl.complain(complainStatus[rand() % 4]);
+
+	cout << endl;
+	cout << MAGENTA "Generating a non-existant complain status..." RESET << endl;
+	Harl.complain("lmao123");
 }
